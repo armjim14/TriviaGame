bf4e8f3b84ff6e1d48f149ca50597d25317b471a
armjim14 <ajimenez1497@gmail.com>
2019-05-29 14:32:07 -0700

    Now need more questions and need to work on questions as well

--- assets/javascript/javas.js
+++ assets/javascript/javas.js
@@ -11,43 +11,49 @@ var intra = "";
 var count = 10;
 var score = 0;
 
+var run = true;
+var run2 = false;
+
 intra = setInterval(function() {
     timer.innerText = " " + count;
     count--;
-    one.textContent = "1"
-    two.textContent = "5"
-    three.textContent = "9"
-    four.textContent = "2"
+    one.textContent = "1";
+    two.textContent = "5";
+    three.textContent = "9";
+    four.textContent = "2";
     question.textContent = "What is 1 + 1?";
 
     for (var i = 0; i < all.length; i++) {
         all[i].addEventListener("click", function() {
             data = this.dataset.number;
-            if (data == "1" || data == "2" || data == "3"){
-                clearInterval(intra);
-                count = 7;
-                wrongone();
-            } else if ( data == "4") {
-                score++;
-                clearInterval(intra);
-                count = 7;
-                rightone();
+            if (run == true){
+                if (data == "1" || data == "2" || data == "3"){
+                    run = false;
+                    run2 = true;
+                    clearInterval(intra);
+                    count = 3;
+                    presecond();
+                } else if ( data == "4") {
+                    score++;
+                    run2 = true;
+                    run = false;
+                    clearInterval(intra);
+                    count = 3;
+                    presecond();
+                }
             }
         })
     }
     if ( count == -1 ){
         clearInterval(intra);
-        count = 7;
-        wrongone();
+        run = false;
+        run2 = true;
+        count = 3;
+        presecond();
     }
 }, 1000)
 
-function rightone() {
-    alert("RIGjt!!");
-    second();
-}
-
-function wrongone() {
+function presecond() {
     intra = setInterval(function() {
         four.style.background = "lightgreen";
         question.textContent = "The Answer was 2";
@@ -70,19 +76,119 @@ function wrongone() {
     }, 1000);
 }
 
-
 function second() {
     intra = setInterval(function() {
+        for ( var i = 0; i < all.length; i++ ){
+            all[i].style.background = "lightgray";
+        }
         timer.innerText = " " + count;
         count--;
-        question.textContent = "Did it work?"
+        one.textContent = "10";
+        two.textContent = "8";
+        three.textContent = "12";
+        four.textContent = "11";
+        question.textContent = "5 + 5 ?";
+
+        for (var i = 0; i < all.length; i++) {
+            all[i].addEventListener("click", function() {
+                data = this.dataset.number;
+                if (run2 == true){
+                    if (data == "4" || data == "2" || data == "3"){
+                        run2 = false;
+                        run3 = true;
+                        clearInterval(intra);
+                        count = 3;
+                        prethird();
+                    } else if ( data == "1") {
+                        score++;
+                        run2 = false;
+                        run3 = true;
+                        clearInterval(intra);
+                        count = 3;
+                        prethird();
+                    }
+                }
+            })
+        }
+
         if (count == -1) {
             clearInterval(intra);
-            third();
+            run2 = false;
+            run3 = true;
+            count = 3;
+            prethird();
         }
     }, 1000)
 }
 
+function prethird() {
+    intra = setInterval(function() {
+        one.style.background = "lightgreen";
+        question.textContent = "The Answer was 10";
+        timer.innerText = " " + count;
+        count --;
+
+        if (data == "4"){
+            one.style.background = "lightcoral";
+        } else if (data == "2"){
+            two.style.background = "lightcoral";
+        } else if (data == "3"){
+            three.style.background = "lightcoral";
+        }
+
+        if (count == 0){
+            clearInterval(intra);
+            count = 10;
+            third();
+        }
+    }, 1000);
+}
+
 function third() {
-    alert("ohhhh");
+    intra = setInterval(function() {
+        for ( var i = 0; i < all.length; i++ ){
+            all[i].style.background = "lightgray";
+        }
+        timer.innerText = " " + count;
+        count--;
+        one.textContent = "8";
+        two.textContent = "2";
+        three.textContent = "4";
+        four.textContent = "6";
+        question.textContent = "8 - 4"
+
+        for (var i = 0; i < all.length; i++) {
+            all[i].addEventListener("click", function() {
+                data = this.dataset.number;
+                if (run3 == true){
+                    if (data == "4" || data == "2" || data == "1"){
+                        run3 = false;
+                        clearInterval(intra);
+                        count = 3;
+                        prefourth();
+                    } else if ( data == "3") {
+                        score++;
+                        run3 = false;
+                        clearInterval(intra);
+                        count = 3;
+                        prefourth();
+                    }
+                }
+            })
+        }
+
+        if (count == -1) {
+            clearInterval(intra);
+            run3 = false;
+            count = 3;
+            prefourth();
+        }
+    }, 1000)
+}
+
+function prefourth() {
+    for ( var i = 0; i < all.length; i++ ){
+        all[i].style.background = "lightgray";
+    }
+    alert("done");
 }
