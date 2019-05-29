29040548513291dd38f822ec68e906ce8c3eca52
armjim14 <ajimenez1497@gmail.com>
2019-05-29 12:16:45 -0700

    what is answer screen

--- assets/javascript/javas.js
+++ assets/javascript/javas.js
@@ -1,14 +1,55 @@
 var timer = document.getElementById("timer");
 var question = document.getElementById("question");
+var one = document.getElementById("one");
+var two = document.getElementById("two");
+var three = document.getElementById("three");
+var four = document.getElementById("four");
+var all = document.querySelectorAll(".all");
+
+var data = "";
 var intra = "";
 var count = 10;
 
 intra = setInterval(function() {
     timer.innerText = " " + count;
     count--;
+    one.textContent = "no"
+    two.textContent = "yes"
+    three.textContent = "maybe"
+    four.textContent = "game"
     question.textContent = "What is my name?";
-    if ( count == 0 ){
-        clearInterval(intra);
-    }
 
+    for (var i = 0; i < all.length; i++) {
+        all[i].addEventListener("click", function() {
+            data = this.dataset.number;
+            if (data == "1" || data == "2" || data == "3"){
+                clearInterval(intra);
+                alert("wrong")
+            } else if ( data == "4") {
+                clearInterval(intra);
+                alert("right")
+            }
+        })
+    }
+    if ( count == -1 ){
+        clearInterval(intra);
+        count = 10;
+        second();
+    }
 }, 1000)
+
+function second() {
+    intra = setInterval(function() {
+        timer.innerText = " " + count;
+        count--;
+        question.textContent = "Did it work?"
+        if (count == -1) {
+            clearInterval(intra);
+            third();
+        }
+    }, 1000)
+}
+
+function third() {
+    alert("ohhhh");
+}
